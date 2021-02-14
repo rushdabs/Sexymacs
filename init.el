cb38d2717786a7051d1780e613f8ea2df45d0915
rushdabs <rushi.kallakuri@gmail.com>
2021-02-14 16:28:44 +0530

    Add autocomplete support with lsp-mode and company.

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
 (set-fontset-font "fontset-default" 'unicode "Noto Color Emoji")
@@ -20,3 +21,16 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-ivy lsp-ui ccls company-lsp lsp-mode company-irony yasnippet which-key use-package rainbow-delimiters org-bullets ivy-rich helpful forge flycheck evil doom-themes doom-modeline counsel-projectile company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
